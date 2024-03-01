aed34730ffad9ae0de288753f8d67b18b0f1f23b
theparulverma <109349527+theparulverma@users.noreply.github.com>
2024-03-01 17:21:01 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,20 @@
+let score = "33"
+ console.log(typeof score);
+
+ console.log(typeof(score));
+
+ let valueInNumber = Number(score);
+ console.log(typeof valueInNumber);
+// string "33" easily be converted into number
+// but string "33abc" can't be converted into number soo it will show nan => it means not a number
+// true => 1,false=>0
+  let islogged=1
+ let booleanislogged = Boolean(islogged);
+ console.log(booleanislogged);
+
+//" " string will be false and any name it be converted into true
+// "" => false
+//"parul" => truue
+let some=33
+let change = String(some);
+console.log(change);
