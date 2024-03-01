cefa56d5586ffa6929de6c115f9f4118c70a824d
theparulverma <109349527+theparulverma@users.noreply.github.com>
2024-03-01 18:37:38 +0000

    operation changes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -18,3 +18,33 @@ let score = "33"
 let some=33
 let change = String(some);
 console.log(change);
+
+//*******operations********** */
+
+let value =3
+let negvalue =-value
+console.log(negvalue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2/2);
+//console.log(2%2);
+
+let str1 = "parul";
+let str2="prabhat";
+let str3 = str1+str2;
+console.log(str3);
+
+console.log(1+"2");
+console.log("1"+2);
+console.log("1"+2+2);//in this pehle hume string mein mila tha soo isliye 122 answer aayega
+
+console.log(1+2+"2");//32
+console.log(+true);
+console.log(+"");
+let num1,num2,num3
+num1=num2=2//it is not a good way of writing the code as in future someone will read your code,it will become difficult
+let gamecouner =100;
+++gamecouner;
+console.log(gamecouner);
