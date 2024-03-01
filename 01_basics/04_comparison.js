c688dabfb0cacff514bff69d72a2cb1d01d93b23
theparulverma <109349527+theparulverma@users.noreply.github.com>
2024-03-01 18:53:01 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,19 @@
+//console.log(2>1); it will return boolean data type
+//console.log(2>=1);
+//console.log(2 < 1);
+//console.log(2==1);
+//console.log(2 != 1);
+//it is wrong if we compare 2 different data types value 
+
+
+//example, bcoz in some of the cases it will return wrong output
+
+//console.log("2" >1);
+//console.log("02" >1);
+
+ console.log(null >0);//return false
+console.log(null == 0);//return false;
+console.log(null >= 0);//true
+//it can be checked with undefinded that is greater or smaller or eual to 0 ,it will always return false;
+// === it not only checks the value but also the datatype
+console.log("4" === 4);
