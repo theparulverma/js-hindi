edf263a4fd039f9780b7fe6ef739de945a5c8b5c
theparulverma <109349527+theparulverma@users.noreply.github.com>
2024-03-05 19:30:35 +0000

    change

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,13 @@
+const name ="Parul"
+const repoCount =50
+//console.log(name + repoCount +  " value"); THIS is not as code readiblity
+//for good version  we use this syntax 
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('parulverma')
+console.log(gameName[0]);//it means parulverma ka p 0th index pe h
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('p'));
