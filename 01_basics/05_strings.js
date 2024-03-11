e71983972c37a2dc18b2dad75ec020d00c272468
theparulverma <109349527+theparulverma@users.noreply.github.com>
2024-03-11 08:49:01 +0000

    done

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -2,12 +2,30 @@ const name ="Parul"
 const repoCount =50
 //console.log(name + repoCount +  " value"); THIS is not as code readiblity
 //for good version  we use this syntax 
-console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);//new version of writing string code
 
-const gameName = new String('parulverma')
+const gameName = new String('parul.verma.29')
 console.log(gameName[0]);//it means parulverma ka p 0th index pe h
 console.log(gameName.__proto__);
 console.log(gameName.length);
 console.log(gameName.toUpperCase());
 console.log(gameName.charAt(2));
 console.log(gameName.indexOf('p'));
+
+const newstring = gameName.substring(0,4)
+console.log(newstring);
+
+const another = gameName.slice(-8,4)
+console.log(another);
+
+const string1 = "  hitesh "
+console.log(string1);
+console.log(string1.trim());
+
+const url ="https://parul.com/parul%29verma"
+
+console.log(url.replace('%29','-'))
+
+console.log(url.includes('sundar'))//it returns a boolean value
+
+console.log(gameName.split('.'));
