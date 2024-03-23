7f2f09a601771ea884b0280e09393a70ce81d2e1
theparulverma <109349527+theparulverma@users.noreply.github.com>
2024-03-23 19:31:50 +0000

    function

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,20 @@
+
+function sayName(){
+    console.log("P");
+    console.log("A");
+    console.log("R");
+    console.log("U");
+    console.log("L");
+}
+//sayName()
+ 
+// function addTwoNumbers(number1, number2){//function definition time when we write in brackets is parameters
+//     console.log(number1 + number2);//in this we name it as arguments
+// }
+
+function addTwoNumbers(number1, number2){//function definition time when we write in brackets is parameters
+    let result = number1 + number2
+    return result// after returning the value in function,it never prints anything
+}
+const result = addTwoNumbers(3,4)//7 
+console.log("Result", result);// in this result value will be undefined
