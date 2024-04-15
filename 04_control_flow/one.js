8ddaa440f0c085d2263f8da8fd1708d2b66430bf
theparulverma <109349527+theparulverma@users.noreply.github.com>
2024-04-15 19:28:41 +0000

    one

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,6 +1,12 @@
 //if
+const isUserLoggedin = true
+const temperature =41
 
-
-if(true){
-    
+if(temperature < 50){
+    console.log("less than 50");
+}
+console.log("temperature is greater than 50");
+if(2 == "2"){//yeh sirf normal check karne ke liye
+    //for strict checking jismein data type bhi same ho uske liye we use '==='
+    console.log("executed");
 }
